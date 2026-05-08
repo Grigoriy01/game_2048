f9cef0b454845a92d6441e917f6c58e5489e388a
Grigoriy <m9258923028@gmail.com>
2026-05-08 19:26:41 +0200

    new structur of file/ without public

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,3 @@
 module.exports = {
-  extends: [
-    '@mate-academy/eslint-config',
-    'plugin:cypress/recommended',
-    'prettier',
-  ],
-
-  rules: {
-    // Переопределяем кавычки на одинарные
-    quotes: ['error', 'single'],
-    // Если линтер ругается на точки с запятой, тоже можно зафиксировать:
-    semi: ['error', 'always'],
-  },
+  extends: ['@mate-academy/eslint-config', 'plugin:cypress/recommended'],
 };
